f75841b2129fc103d39bac43b95f2cb117d49080
Phone Min Myat <phoneminmyat102115@gmail.com>
2026-03-26 14:59:36 +0630

    change cors config

--- config/cors.php
+++ config/cors.php
@@ -6,9 +6,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://uat-ws.ngwekhaing.com',
-        'https://uat-user.ngwekhaing.com',
-        'https://uat-admin.ngwekhaing.com',
+        "*"
     ],
 
     'allowed_headers' => [
